dac5abdbd90f09999f4e086e174103cfa5180fb8
MIELYX <kacper_mielczarek@Kacpers-Air.lan>
2022-11-07 18:20:29 +0100

    traveloo v.6

--- js/script.js
+++ js/script.js
@@ -3,10 +3,9 @@ const nav = document.querySelector('.nav__slide-out')
 const items = document.querySelectorAll('.item')
 
 const slide = () => {
-    nav.classList.toggle('slide')
-    document.body.classList.toggle('sticky')
+	nav.classList.toggle('slide')
+	document.body.classList.toggle('sticky')
 }
-
 
 burger.addEventListener('click', slide)
 items.forEach(item => item.addEventListener('click', slide))
